c7d07b659619552223f35eced19808e0c12ce3c7
Joako <joaquin.vannucci202875@alumnos.info.unlp.edu.ar>
2022-07-08 18:39:28 -0300

    agregando los prompt

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const iva = 0.21;
 const impues = 0.5;
-let cantAutos = 0; //Esta es una variable para saber cuantos autos eligio el cliente.
-let precioAuto = 0;
-let cuotas = 12;
+let cantAutos = parseInt(prompt("¿Cuantos autos va a comprar?")); //Esta es una variable para saber cuantos autos eligio el cliente.
+let precioAuto = parseFloat(prompt("¿Cuanto cuesta el auto?"));
+let cuotas = parseInt(prompt("¿Cuantas cuotas desea pagar? minimo 4, maximo 24. (tenga en cuenta que si ingresa un numero mayor a 24, se realizara un solo pago)"));
 
 
 const precioTotal = (cantAutos) => {  //funcion que calcula (precio auto + iva) + impuestos.
@@ -13,7 +13,7 @@ const precioTotal = (cantAutos) => {  //funcion que calcula (precio auto + iva) 
   }
 }
 
-function cuantasCuotas(){
+function cuantasCuotas(cuotas){
   switch (cuotas) {
     case 4:
       console.log(`4 pagos mensuales de ${precioTotal(cantAutos)/4}`);
@@ -28,3 +28,4 @@ function cuantasCuotas(){
         console.log(`Se realiza un solo pago de ${precioTotal(cantAutos)}`);
   } 
 }
+
