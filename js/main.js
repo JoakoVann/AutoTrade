1a131b5fe94f60ccc8632f9884356e45e767baa1
Joako <joaquin.vannucci202875@alumnos.info.unlp.edu.ar>
2022-07-13 19:33:31 -0300

    reentrega

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-const iva = 0.21;
-const impues = 0.5;
-let cantAutos = parseInt(prompt("¿Cuantos autos va a comprar?")); //Esta es una variable para saber cuantos autos eligio el cliente.
-let precioAuto = parseFloat(prompt("¿Cuanto cuesta el auto?"));
-let cuotas = parseInt(prompt("¿Cuantas cuotas desea pagar? minimo 4, maximo 24. (tenga en cuenta que si ingresa un numero mayor a 24, se realizara un solo pago)"));
+const iva = 1.21;
+const impues = 1.5;
+let cantAuto = parseInt(prompt("¿Cuantos autos va a comprar?"));
+let precio = 0;
 
-
-const precioTotal = (cantAutos) => {  //funcion que calcula (precio auto + iva) + impuestos.
-  for (cantAutos; cantAutos > 0; cantAutos--) {
-    let precio = 0;
-    precio = (precio * iva)*impues;
-    return precio;
+let priceTot = () => {
+  for (cantAuto; cantAuto > 0; cantAuto--) {
+    let precioAuto = parseFloat(prompt(`¿Cuanto cuesta el auto ${cantAuto} ?`));
+    precio = precio + (precioAuto * iva)* impues;
   }
+  return precio;
 }
 
-function cuantasCuotas(cuotas){
+function cuantasCuotas(){
+  let cuotas = parseInt(prompt("¿Cuantas cuotas desea pagar? minimo 4, maximo 24. (tenga en cuenta que si ingresa un numero mayor a 24, se realizara un solo pago)"));
   switch (cuotas) {
     case 4:
-      console.log(`4 pagos mensuales de ${precioTotal(cantAutos)/4}`);
+      console.log(`4 pagos mensuales de ${priceTot()/4}`);
     break;
     case 12:
-      console.log(`12 pagos mensuales de ${precioTotal(cantAutos)/12}`);
+      console.log(`12 pagos mensuales de ${priceTot()/12}`);
     break;
     case 24:
-      console.log(`24 pagos mensuales de ${precioTotal(cantAutos)/24}`);
+      console.log(`24 pagos mensuales de ${priceTot()/24}`);
     break;
     default:
-        console.log(`Se realiza un solo pago de ${precioTotal(cantAutos)}`);
+        console.log(`Se realiza un solo pago de ${priceTot()}`);
   } 
 }
-
+priceTot();
+cuantasCuotas();
