b19a1ceba6fe4d58b549d464f0f4735ecf2a4f38
JoakoVann <joaquin.vannucci202875@alumnos.info.unlp.edu.ar>
2022-07-26 18:10:05 -0300

    finalizando desafio

--- js/main.js
+++ js/main.js
@@ -1,55 +1,74 @@
+//CONSTANTES
+
 const iva = 1.21;
 const impues = 1.5;
-let precio = 0;
 
+// OBJETO DE COMPRA Y CLIENTE
 
-function cuantasCuotas(){
-  let cuotas = parseInt(prompt("¿Cuantas cuotas desea pagar? minimo 4, maximo 24. (tenga en cuenta que si ingresa un numero mayor a 24, se realizara un solo pago)"));
-  switch (cuotas) {
-    case 4:
-      console.log(`4 pagos mensuales de ${priceTot()/4}`);
-    break;
-    case 12:
-      console.log(`12 pagos mensuales de ${priceTot()/12}`);
-    break;
-    case 24:
-      console.log(`24 pagos mensuales de ${priceTot()/24}`);
-    break;
-    default:
-        console.log(`Se realiza un solo pago de ${priceTot()}`);
-  } 
+class cliente {
+  constructor(nombre,apellido,email,cantCompras){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.cantCompras = cantCompras;
+  }
 }
-priceTot();
-cuantasCuotas();
-
-class compra {
-  constructor(modelo, marca, precio){
-    this.modelo = modelo;
+class producto{
+  constructor(marca,modelo,precio){
     this.marca = marca;
+    this.modelo = modelo;
     this.precio = precio;
   }
-  sumaPrecio(){
-    this.precio = this.precio + (this.precio * iva)* impues;
-  }
 }
 
-const compras = [];
+// VARIABLES GLOBALES
 
-function cliente (compra){ 
-  let confirma = prompt("¿Desea agregar una compra? (si/no)").toLowerCase();
-  if (confirma === "si") {
-    while (!confirma){
-      compras.push (new compra(prompt("Ingrese el modelo"), prompt("Ingrese la marca"), parseInt(prompt("Ingrese el precio"))));
-      confirma = prompt("¿Desea agregar otra compra? (si/no)").toLowerCase();
-      if (confirma === "no") {
-        break;
-      }
-      for (const compra of compras){
-        compra.sumaPrecio();
-      }
-    }
-  }
-  return compra;
-} //esta funcion lo que haria seria por cada vez que se haga click en el boton de "comprar ahora", vaya creando un array con cada compra realizada por el cliente
+let productos = [];
+let prodComprados = [];
+let totalComprados = [];
+let total = 0;
+const nombre = document.querySelector('#nombre');
+const apellido = document.querySelector('#apellido');
+const email = document.querySelector('#email');
+// Agregar al carrito
+const finCompra = document.getElementById('flotante');
+const comprar = document.querySelector('.compra');
+const marca = document.querySelector('.marca');
+const modelo = document.querySelector('.modelo');
+const price = document.querySelector('.precio');
+let getElementById = document.getElementById('cocheSelect');
 
-cliente(compra);
+comprar.addEventListener('click', (e) => {
+  e.preventDefault();
+  prodComprados.push(new producto(marca.innerHTML,modelo.innerHTML,parseInt(price.innerHTML)));
+  finCompra.style.display = 'block';
+  alert('vehiculo agregado correctamente');
+})
+// Cosas del HTML y estilos
+const compra = document.createElement('section');
+compra.innerHTML = `
+<div class="price">        
+  <label class="ms-2 mt-1" for="floatingInput">Precio del vehiculo</label>
+  <input type="text" class="form-control ingresar" id="floatingInput" placeholder="precio Vehiculo" readonly>
+  <label class="ms-2 mt-1" for="floatingInput">21%</label>
+  <input type="text" class="form-control ingresar" id="floatingInput" placeholder="Precio Vehiculo + Impuestos" readonly>
+  <label class="ms-2 mt-1" for="precioTot">${(parseInt(price.innerHTML * iva)*impues)}</label>
+</div>
+<div class="cuotas">
+  <label for="cuotas">Cantidad de Cuotas</label>
+  <select name="" id="cuotas">
+    <option value="4">${(parseInt(price.innerHTML * iva)*impues)/4}</option>
+    <option value="12">${(parseInt(price.innerHTML * iva)*impues)/12}</option>
+    <option value="24">${(parseInt(price.innerHTML * iva)*impues)/24}</option>
+    <option value="32">${(parseInt(price.innerHTML * iva)*impues)/32}</option>
+  </select>
+  <button class="float" id="fin">finalizar compra</button>
+</div>
+`
+
+let final = document.getElementById(cocheSelect);
+
+finCompra.addEventListener('click', (e) => {
+  e.preventDefault();
+  final.append(compra);
+});
